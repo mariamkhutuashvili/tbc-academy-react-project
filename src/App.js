73f05c2e295c39a273a6e70899c25b808c6905aa
Mariam Khutuashvili <m_khutuashvili@yahoo.com>
2024-03-14 00:24:10 +0400

    Split App.js into separate components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import Blog from "./components/Blog";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function toggleTheme() {
@@ -10,69 +12,9 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="header">
-          <div>
-            <button className="button" onClick={toggleTheme}>
-              Switch Theme
-            </button>
-          </div>
-          <nav>
-            <a href="#home" className="nav-link">
-              Home
-            </a>
-            <a href="#about" className="nav-link">
-              About
-            </a>
-            <a href="#services" className="nav-link">
-              Services
-            </a>
-            <a href="#contact" className="nav-link">
-              Contact
-            </a>
-          </nav>
-        </header>
-        <main className="main-content">
-          <h1>Stay Updated with Our Latest News!</h1>
-          <Blog />
-        </main>
-        <footer className="footer">
-          <div className="footer-section">
-            <h3>Quick Links</h3>
-            <div className="footer-nav">
-              <a href="#about" className="footer-link">
-                About
-              </a>
-              <a href="#services" className="footer-link">
-                Services
-              </a>
-              <a href="#contact" className="footer-link">
-                Contact
-              </a>
-            </div>
-          </div>
-          <div className="footer-section">
-            <h3>Information</h3>
-            <a href="#terms" className="footer-link">
-              Terms and Conditions
-            </a>
-            <a href="#privacy" className="footer-link">
-              Privacy Policy
-            </a>
-          </div>
-          <div className="footer-section">
-            <h3>Newsletter</h3>
-            <form>
-              <label htmlFor="email">Subscribe to our newsletter:</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Enter your email"
-              />
-              <button type="submit">Subscribe</button>
-            </form>
-          </div>
-        </footer>
+        <Header toggleTheme={toggleTheme} />
+        <MainContent />
+        <Footer />
       </div>
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+function Header({ toggleTheme }) {
+  return (
+    <header className="header">
+      <div>
+        <button className="button" onClick={toggleTheme}>
+          Switch Theme
+        </button>
+      </div>
+      <nav className="nav">
+        <a href="#home" className="nav-link">
+          Home
+        </a>
+        <a href="#about" className="nav-link">
+          About
+        </a>
+        <a href="#services" className="nav-link">
+          Services
+        </a>
+        <a href="#contact" className="nav-link">
+          Contact
+        </a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
