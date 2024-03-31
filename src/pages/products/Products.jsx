68cfc0fc21b5d620452ad46834793f7795ebac52
Mariam Khutuashvili <m_khutuashvili@yahoo.com>
2024-04-01 01:53:21 +0400

    Implement sorting by price in Products component

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -5,28 +5,36 @@ import "./Products.css";
 
 function Products() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [isSorted, setIsSorted] = useState(false);
 
-  const filteredProducts = productsData.filter(
-    (product) =>
-      product.headline.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      product.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      product.price.toString().includes(searchTerm)
-  );
+  const filteredAndSortedProducts = productsData
+    .filter(
+      (product) =>
+        product.headline.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        product.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        product.price.toString().includes(searchTerm)
+    )
+    .sort((a, b) => (isSorted ? a.price - b.price : 0));
 
   return (
     <div className="store">
       <h1>Refresh & Refill Online Store</h1>
       <Search onChange={(e) => setSearchTerm(e.target.value)} />
+      <button onClick={() => setIsSorted(!isSorted)} className="button">
+        {isSorted ? "Reset" : "Sort by Price"}
+      </button>
       <div className="products-grid">
-        {filteredProducts.map(({ id, headline, description, photo, price }) => (
-          <div key={id} className="product">
-            <img src={photo} alt={headline} />
-            <h2>{headline}</h2>
-            <p>{description}</p>
-            <p className="product-price">${price.toFixed(2)}</p>
-            <button className="button">Add to Cart</button>
-          </div>
-        ))}
+        {filteredAndSortedProducts.map(
+          ({ id, headline, description, photo, price }) => (
+            <div key={id} className="product">
+              <img src={photo} alt={headline} />
+              <h2>{headline}</h2>
+              <p>{description}</p>
+              <p className="product-price">${price.toFixed(2)}</p>
+              <button className="button">Add to Cart</button>
+            </div>
+          )
+        )}
       </div>
     </div>
   );
