bea0a6d4e1d503f28f4a41683102e22913c7e67b
Mariam Khutuashvili <m_khutuashvili@yahoo.com>
2024-04-22 10:49:03 +0400

    Remove redundant lines for cleaner functionality

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,22 +1,10 @@
 "use server";
 
-// import { AUTH_COOKIE_KEY } from "../../constants";
-// import { redirect } from "next/navigation";
-// import { cookies } from "next/headers";
 import { login } from "../actions";
 import LoginForm from "../../components/LoginForm/LoginForm";
 import "../../styles/Login.css";
 
 export default async function Login() {
-  // const cookieStore = cookies();
-  // const cookie = cookieStore.get(AUTH_COOKIE_KEY);
-
-  // console.log("cookie:", cookie);
-
-  // if (cookie?.value) {
-  //   redirect("/");
-  // }
-
   const handleLogin = async (username, password) => {
     "use server";
     await login(username, password);
