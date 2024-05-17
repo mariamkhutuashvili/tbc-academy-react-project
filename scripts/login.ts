7339725600bf4e997ee309e955820a6acea421dc
mariamkhutuashvili <m_khutuashvili@yahoo.com>
2024-05-18 00:20:17 +0400

    Change localhost:3000 for environment flexibility

--- scripts/login.ts
+++ scripts/login.ts
@@ -1,5 +1,5 @@
 export const handleLogin = async (username: string, password: string) => {
-  await fetch("http://localhost:3000/api/login", {
+  await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
